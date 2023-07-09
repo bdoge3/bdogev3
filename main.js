204e1f633858a0ca3ef16cb2bc0bc9c301755d08
bdoge3 <139039478+bdoge3@users.noreply.github.com>
2023-07-09 14:31:21 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+const chartData = {
+  labels: ["Presale", "Liquidity", "Burn", ],
+  data: [57, 27.6165, 15.3835],
+};
+
+const myChart = document.querySelector(".my-chart");
+const ul = document.querySelector(".programming-stats .details ul");
+
+new Chart(myChart, {
+  type: "doughnut",
+  data: {
+    labels: chartData.labels,
+    datasets: [
+      {
+        label: " ",
+        data: chartData.data,
+      },
+    ],
+  },
+  options: {
+    borderWidth: 7,
+    borderRadius: 2,
+    hoverBorderWidth: 0,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
+  },
+});
+
+const populateUl = () => {
+  chartData.labels.forEach((l, i) => {
+    let li = document.createElement("li");
+    li.innerHTML = `${l}: <span class='percentage'>${chartData.data[i]}%</span>`;
+    ul.appendChild(li);
+  });
+};
+
+populateUl();
